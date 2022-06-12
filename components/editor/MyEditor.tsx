aa38a15668a7d4aa4176aa29cde529dba10370b3
nut1414 <nut.pannapat@gmail.com>
2022-06-13 01:25:00 +0700

    editor: more tools

--- components/editor/MyEditor.tsx
+++ components/editor/MyEditor.tsx
@@ -1,16 +1,39 @@
-import { createReactEditorJS } from "react-editor-js";
+import { createReactEditorJS } from "react-editor-js"
+import Table from '@editorjs/table'
 import Paragraph from '@editorjs/paragraph'
+import List from '@editorjs/list'
+import Warning from '@editorjs/warning'
+import Code from '@editorjs/code'
+import LinkTool from '@editorjs/link'
+import Header from '@editorjs/header'
+import Quote from '@editorjs/quote'
+import Marker from '@editorjs/marker'
+import CheckList from '@editorjs/checklist'
+import Delimiter from '@editorjs/delimiter'
+import InlineCode from '@editorjs/inline-code'
+import SimpleImage from '@editorjs/simple-image'
 import { useEffect } from 'react'
 
 const EditorJs = createReactEditorJS()
 
 const MyEditor = ({handleInitialize, onChange, defaultValue}: any) => {
   const EDITOR_JS_TOOLS = {
-    paragraph: Paragraph
+    table: Table,
+    list: List,
+    warning: Warning,
+    code: Code,
+    linkTool: LinkTool,
+    header: Header,
+    quote: Quote,
+    marker: Marker,
+    checklist: CheckList,
+    delimiter: Delimiter,
+    inlineCode: InlineCode,
+    simpleImage: SimpleImage,
   }
   
   return (
-    <EditorJs defaultValue={defaultValue} onChange={onChange} onInitialize={handleInitialize} />
+    <EditorJs tools={EDITOR_JS_TOOLS} defaultValue={defaultValue} onChange={onChange} onInitialize={handleInitialize} />
   )
 }
 
